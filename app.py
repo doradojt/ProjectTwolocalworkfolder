658be87cfd007cfe72cfb912adab2cf5a21726a6
John Michals <jmichals@Dorado.Local>
2018-12-14 11:50:57 -0500

    addition

--- app.py
+++ app.py
@@ -55,30 +55,31 @@ def forecastavg():
     all_avgs = list(np.ravel(results))
 
     return jsonify(all_avgs)
-# @app.route("/forecast")
-# def forecast():
-#     """Return a list of all data """
-#
-#     results = session.query(Data).all()
 
-#
-#     all_data = []
-#     for data in results:
-#         data_dict = {}
-#         data_dict["state_id"] = data.state_id
-#         data_dict["station_name"] = data.station_name
-#         data_dict["state"] = data.state
-#         data_dict["country"] = data.country
-#         data_dict["production_date"] = data.production_date
-#         data_dict["forecast_date"] = data.forecast_date
-#         data_dict["lat"] = data.lat
-#         data_dict["lon"] = data.lon
-#         data_dict["fcst_avg"] = data.fcst_avg
-#         data_dict["norm_mn"] = data.norm_mn
-#         data_dict["norm_mx"] = data.norm_mx
-#         all_data.append(data_dict)
 
-#     return jsonify(all_data)
+###@app.route("/alldata")
+#def alldata():
+#attempt to return all data
+
+ #   results = session.query(Data).all()
+ #   data_dict = {}
+
+  #  for data in results:
+  #       data_dict["state_id"] = data.state_id
+  #       data_dict["station_name"] = data.station_name
+  #       data_dict["state"] = data.state
+  #       data_dict["country"] = data.country
+  #       data_dict["production_date"] = data.production_date
+  #       data_dict["forecast_date"] = data.forecast_date
+  #       data_dict["lat"] = data.lat
+  #       data_dict["lon"] = data.lon
+  #       data_dict["fcst_avg"] = data.fcst_avg
+   #      data_dict["norm_mn"] = data.norm_mn
+   #      data_dict["norm_mx"] = data.norm_mx
+   #      data.append(data_dict)
+    
+   # return jsonify(data_dict)###
+
    
 if __name__ == "__main__":
     app.run(debug=True)
